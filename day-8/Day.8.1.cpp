86c9467456e2288683e0bf86511131625a696199
Vaibhav Kumar <vaibhavkumar5158@gmail.com>
2025-05-07 14:39:33 +0530

    Day 8 added

---
+++ day-8/Day.8.1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+    public:
+        int titleToNumber(string columnTitle) {
+            int result = 0;
+            
+           
+            for (char c : columnTitle) {
+               
+                result = result * 26;
+                
+                result += (c - 'A' + 1);
+            }
+            
+            return result;
+        }
+    };
+    
